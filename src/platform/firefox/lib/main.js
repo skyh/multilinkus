31a9a5ac88ad7ed2f71a1588cc4e25ff56f84cc1
Andrey Ivlev <skyh@ya.ru>
2015-03-11 14:10:35 +0300

    (firefox) Inject content script on install

--- src/platform/firefox/lib/main.js
+++ src/platform/firefox/lib/main.js
@@ -3,19 +3,45 @@
 var api = require('mtl/core/api');
 var tabs = require('sdk/tabs');
 var pageMod = require('sdk/page-mod');
+var self = require('sdk/self');
+
+
+var contentScriptFile = './ui.js';
+
+function listenTabMessages(port) {
+	port.on('openLinks', function (links) {
+		var linksForOpening = api.filterLinks(links);
+		linksForOpening.forEach(function (link) {
+			tabs.open({
+				url: link,
+				inBackground: true
+			});
+		});
+	});
+}
 
 pageMod.PageMod({
 	include: '*',
-	contentScriptFile: './ui.js',
+	contentScriptFile: contentScriptFile,
 	onAttach: function (worker) {
-		worker.port.on('openLinks', function (links) {
-			var linksForOpening = api.filterLinks(links);
-			linksForOpening.forEach(function (link) {
-				tabs.open({
-					url: link,
-					inBackground: true
-				});
-			});
-		});
+		listenTabMessages(worker.port);
 	}
 });
+
+var loadReason = self.loadReason;
+var initUrlTest = /https?:/i;
+
+if (loadReason === 'install') {
+	for (var i in tabs) {
+		var tab = tabs[i];
+		var url = tab.url;
+
+		if (true || initUrlTest.test(url)) {
+			var worker = tab.attach({
+				contentScriptFile: contentScriptFile
+			});
+
+			listenTabMessages(worker.port);
+		}
+	}
+}
