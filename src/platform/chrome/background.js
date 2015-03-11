96a5d147ba9062e664e9f0bbe74c31ccb706cec7
Andrey Ivlev <skyh@ya.ru>
2015-03-11 11:46:31 +0300

    (chrome) Inject content scripts on install

--- src/platform/chrome/background.js
+++ src/platform/chrome/background.js
@@ -24,3 +24,55 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 	actions[action].apply(actions, args);
 });
+
+
+chrome.runtime.onInstalled.addListener(function (details) {
+	var reason = details.reason;
+
+	if (reason === 'install') {
+		injectContentScriptsInAllMatchedTabs();
+	}
+});
+
+
+function injectContentScriptsInAllMatchedTabs() {
+	var manifest = chrome.runtime.getManifest();
+	var contentScripts = manifest['content_scripts'];
+
+	if (contentScripts && contentScripts.length > 0) {
+		for (var i = 0, l = contentScripts.length; i < l; ++i) {
+			var contentScript = contentScripts[i];
+			injectContentScript(contentScript);
+		}
+	}
+}
+
+
+function injectContentScript(script) {
+	var query = {url: script.matches};
+
+	chrome.tabs.query(query, function (tabs) {
+		for (var i = tabs.length; i--;) {
+			var tab = tabs[i];
+
+			var scriptFiles = script.js;
+			var currentFile = 0;
+
+			(function next() {
+				var file = scriptFiles[currentFile++];
+
+				if (!file) {
+					return;
+				}
+
+				var executeOptions = {
+					file: file
+				};
+
+				chrome.tabs.executeScript(tab.id, executeOptions, function (results) {
+					next();
+				});
+			}());
+		}
+	});
+}
